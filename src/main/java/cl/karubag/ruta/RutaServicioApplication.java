b6877b6a6a25fb484d69c2e7326c32250583cfe3
klaw3134 <cgnavarrete3134@gmail.com>
2026-05-28 18:16:28 -0400

    ruta-servicio

---
+++ src/main/java/cl/karubag/ruta/RutaServicioApplication.java
@@ -0,0 +1,13 @@
+package cl.karubag.ruta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutaServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutaServicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/karubag/ruta/RutaServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.karubag.ruta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutaServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
